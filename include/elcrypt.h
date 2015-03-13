66c8d67b1c68b03611e7e5e195370e54d7e307b5
tran_0 <david.tran@epitech.eu>
2015-03-13 23:41:19 +0100

    push header ++ and Feistel init

--- include/elcrypt.h
+++ include/elcrypt.h
@@ -5,22 +5,23 @@
 ** Login   <tran_0@epitech.net>
 ** 
 ** Started on  Fri Mar 13 21:05:54 2015 David Tran
-** Last update Fri Mar 13 21:49:02 2015 David Tran
+** Last update Fri Mar 13 23:40:04 2015 David Tran
 */
 
 #ifndef ELCRYPT_H_
 # define ELCRYPT_H_
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <unistd.h>
 
 typedef union		u_key
 {
   unsigned long int	key;
   unsigned char		c_key[64];
-  unsigned int		i_key[16];
+  unsigned int		i_key[2];
+  unsigned short int	s_key[];
 }			t_key;
 
 typedef struct		s_crypt
@@ -29,6 +30,7 @@ typedef struct		s_crypt
   int			fdin;
   int			fdout;
   t_key			key;
+  t_key			tmp;
 }			t_crypt;
 
 #endif /* !ELCRYPT_H_ */
---
+++ src/Feistel.c
@@ -0,0 +1,25 @@
+/*
+** Feistel.c for Feistel in /home/tran_0/rendu/elcrypt
+** 
+** Made by David Tran
+** Login   <tran_0@epitech.net>
+** 
+** Started on  Fri Mar 13 23:11:08 2015 David Tran
+** Last update Fri Mar 13 23:40:33 2015 David Tran
+*/
+
+void	make_turns(t_crypt *crypt)
+{
+  int	i;
+  int	first;
+  int	second;
+
+  i = 0;
+  first = crypt->tmp.i_key[0];
+  second = crypt->tmp.i_key[1];
+  while (i < 8)
+    {
+      
+      i++;
+    }
+}
