7ef09e1796b3eff82dbe678c0aae0f238561ca0e
Guillaume 'Guiz' Briard <you@example.com>
2015-03-14 22:29:18 +0100

    update Feistel

--- src/Feistel.c
+++ src/Feistel.c
@@ -1,18 +1,35 @@
 /*
 ** Feistel.c for Feistel in /home/tran_0/rendu/elcrypt
-** 
+**
 ** Made by David Tran
 ** Login   <tran_0@epitech.net>
-** 
+**
 ** Started on  Fri Mar 13 23:11:08 2015 David Tran
 ** Last update Sat Mar 14 15:47:00 2015 David Tran
 */
 
 #include "elcrypt.h"
 
-int	rotate_key(int to_rot)
+int                     rotate_key(t_crypt *crypt, int tour)
 {
-  
+  int                   c1;
+  int                   c2;
+
+  c1 = 0;
+  c2 = 0;
+  crypt->tmp.key = crypt->key.key;
+  while (c1 <= tour)
+    {
+      while (c2 < 4)
+        {
+          if ((crypt->tmp.key >> 63) == 1)
+            crypt->tmp.key |= 1;
+          crypt->tmp.key << 1;
+          c2++;
+        }
+      c1++;
+    }
+  return (EXIT_SUCCESS);
 }
 
 int	build_key(t_crypt *crypt, int i)
@@ -22,15 +39,14 @@ int	build_key(t_crypt *crypt, int i)
 void	make_turns(t_crypt *crypt)
 {
   int	i;
-  int	first;
   int	second;
 
   i = 0;
-  first = crypt->tmp.i_key[0];
-  second = crypt->tmp.i_key[1];
   while (i < 8)
     {
-      
+      rotate_key(crypt->key, i);
       i++;
+      second = crypt->tmp.i_key[1];
+      // Feistel
     }
 }
