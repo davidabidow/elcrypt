71b186a8f23cb182cf800b08efa770b6bacb7f36
Guillaume 'Guiz' Briard <you@example.com>
2015-03-15 02:55:19 +0100

    prenatcon

--- src/encrypt.c
+++ src/encrypt.c
@@ -1,9 +1,9 @@
 /*
 ** encrypt.c for encrypt in /home/tran_0/rendu/elcrypt
-** 
+**
 ** Made by David Tran
 ** Login   <tran_0@epitech.net>
-** 
+**
 ** Started on  Sat Mar 14 21:44:10 2015 David Tran
 ** Last update Sun Mar 15 00:52:17 2015 David Tran
 */
@@ -12,8 +12,19 @@
 
 void	encrypt(t_crypt *crypt, int i)
 {
+  int   r_tmp;
+  int   l_tmp;
+
+  r_tmp = crypt->tmp.i_key[1];
+  l_tmp = crypt->tmp.i_key[0];
   if (i % 2 == 0)
-    crypt->tmp.i_key[0] ^= crypt->second.i_key[1];
+    {
+      r_tmp ^= crypt->second.i_key[1];
+      crypt->tmp.i_key[0] ^= r_tmp;
+    }
   else
-    crypt->tmp.i_key[1] ^= crypt->second.i_key[1];
+    {
+      l_tmp ^= crypt->second.i_key[1];
+      crypt->tmp.i_key[1] ^= l_tmp;
+    }
 }
